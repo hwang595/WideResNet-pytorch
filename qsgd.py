544a06a72c9337353fce62c6eb18202b21aa4b58
hwang595 <hongyiwang@cs.wisc.edu>
2018-01-28 13:25:21 -0600

    clean

--- qsgd.py
+++ qsgd.py
@@ -7,7 +7,6 @@ import time
 # TODO: pass flag (qsgd/terngrad) and clip_factor into kwargs or figure out a better way
 def encode(v, scheme='qsgd', **kwargs):
     w = v.view(-1).numpy()
-    s = None
     if scheme == 'qsgd':
         norm = torch.norm(v)
     elif scheme == 'terngrad':
@@ -26,7 +25,7 @@ def encode(v, scheme='qsgd', **kwargs):
     signs = ((signs + 1) / 2).astype('bool')
 
     code = {'signs': signs, 'size': v.size(), 'selected': selected,
-            'norm': norm, 's':s}
+            'norm': norm}
 
     if kwargs.pop('timings', False):
         data = {}
@@ -69,21 +68,23 @@ def grad_clip_limit(grad, clip_factor=2.5):
 if __name__ == "__main__":
     n = 50
     x = torch.rand(n)
-    scheme='terngrad' # scheme can take value of "terngrad" or "qsgd"
-    repeats = int(10e3)
-    codes = [encode(x, scheme=scheme) for _ in range(repeats)]
-    
-    max_scalar = pre_decode(codes)
-    approxs = [decode(code, **max_scalar) for code in codes]
+    for scheme in ['terngrad', 'qsgd']:
+        # scheme can take value of "terngrad" or "qsgd"
+        print(scheme)
+        repeats = int(10e3)
+        codes = [encode(x, scheme=scheme) for _ in range(repeats)]
+        
+        max_scalar = pre_decode(codes)
+        approxs = [decode(code, **max_scalar) for code in codes]
 
-    data = map(lambda arg: {'y': arg[1], 'norm(y)**2': torch.norm(arg[1])**2,
-                            'len(signs)': len(arg[0]['signs'])},
-               zip(codes, approxs))
-    sums = reduce(lambda x, y: {k: x[k] + y[k] for k in x}, data)
-    avg = {k: v / len(codes) for k, v in sums.items()}
-    assert avg['norm(y)**2'] <= np.sqrt(n) * torch.norm(x)**2
-    # TODO: not need this sanity check for terngrad 
-    #assert avg['len(signs)'] <= np.sqrt(n)
-    rel_error = torch.norm(avg['y'] - x) / torch.norm(x)
-    print(rel_error)
-    assert rel_error < 0.2
+        data = map(lambda arg: {'y': arg[1], 'norm(y)**2': torch.norm(arg[1])**2,
+                                'len(signs)': len(arg[0]['signs'])},
+                   zip(codes, approxs))
+        sums = reduce(lambda x, y: {k: x[k] + y[k] for k in x}, data)
+        avg = {k: v / len(codes) for k, v in sums.items()}
+        assert avg['norm(y)**2'] <= np.sqrt(n) * torch.norm(x)**2
+        if scheme == 'qsgd':
+            assert avg['len(signs)'] <= np.sqrt(n)
+        rel_error = torch.norm(avg['y'] - x) / torch.norm(x)
+        print(rel_error)
+        assert rel_error < 0.2
